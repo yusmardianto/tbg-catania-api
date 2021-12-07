2b3f0c926027f4bf58cb6dbb96b8c2e64ac045ee
root <root@server-c8.thamrin.co.id>
2021-12-07 14:26:26 +0700

    live

--- config/server.js
+++ config/server.js
@@ -3,7 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int('PORT', 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ea704c52f457e83bff1b8a05b9c3a835'),
+      secret: env('ADMIN_JWT_SECRET'),
     },
   },
 });
